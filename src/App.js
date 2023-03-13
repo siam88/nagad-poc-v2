e43efb79f79d0a07667b1422e1dd7e6965afda32
Rafshanul Hoque Siam <rafshanulsiam811@gmail.com>
2023-03-13 10:55:22 +0600

    worked in mobile

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 import { useAddToHomescreenPrompt } from "./AddToHomeScreen";
 import Home from "./pages/index";
@@ -19,22 +19,18 @@ function App() {
   };
   const handleShow = () => setShow(true);
 
-  useEffect(() => {
+  React.useEffect(() => {
     isPWAInstalled();
   }, []);
 
   const isPWAInstalled = async () => {
-    
     if ("getInstalledRelatedApps" in window.navigator) {
       const relatedApps = await navigator.getInstalledRelatedApps();
       let installed = false;
-      console.log("relatedApps",navigator);
-
       relatedApps.forEach((app) => {
         //if your PWA exists in the array it is installed
-
         console.log(app.platform, app.url);
-        if (app.url === "https://nagad-poc-v2.vercel.app/manifest.json") {
+        if (app.url === "https://nagad-poc-v2.netlify.app/manifest.json") {
           installed = true;
         }
       });
@@ -46,22 +42,20 @@ function App() {
   return (
     <>
       <Home />
+      
 
       {!isAppInstalled && (
         <Modal size="sm" centered show={show} onHide={handleClose}>
           <Modal.Body>
             <h4>Install Nagad Poc</h4>
-            <p>
-              Install this application on your home screen for quick and easy
-              access{" "}
-            </p>
+            <p>Install this application on your home screen for quick and easy access </p>
           </Modal.Body>
           <Modal.Footer>
             <Button variant="secondary" onClick={handleClose} size="sm">
               Close
             </Button>
             <Button variant="primary" onClick={handleInstall} size="sm">
-              Install
+              Ok
             </Button>
           </Modal.Footer>
         </Modal>
